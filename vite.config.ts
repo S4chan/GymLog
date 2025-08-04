e0d950519328511c710e57d5ee8310fe8ff49ae3
S4chan <heineken1204@gmail.com>
2025-08-04 16:32:51 +0800

    refactor: update vite.config.ts for improved readability and consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/GymLog/" : "/",
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/GymLog/' : '/',
+    plugins: [vue()],
+  };
 });
