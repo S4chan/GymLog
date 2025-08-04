f82065184a5dc759a87c0308a2637ba18569625c
S4chan <heineken1204@gmail.com>
2025-08-04 16:22:56 +0800

    feat: update package.json and package-lock.json to include gh-pages dependency and related updates; refactor App.vue for improved readability and consistency; modify vite.config.ts for better formatting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import Layout from "./layouts/Layout.vue";
-import { useWorkoutStore } from './stores/workoutStore';
-import { useUserStore } from './stores/userStore';
-import { onMounted } from 'vue';
-import { useRouter } from 'vue-router';
+import { useWorkoutStore } from "./stores/workoutStore";
+import { useUserStore } from "./stores/userStore";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 
 const workoutStore = useWorkoutStore();
 const userStore = useUserStore();
@@ -13,16 +13,11 @@ onMounted(() => {
   workoutStore.initialize_data();
   userStore.initialize();
   if (userStore.name) {
-    router.push({ name: 'Dashboard' });
+    router.push({ name: "Dashboard" });
   } else {
-    router.push({ name: 'Welcome' });
+    router.push({ name: "Welcome" });
   }
 });
-
-function handleLogout() {
-  userStore.logout();
-  router.push({ name: 'Welcome' });
-}
 </script>
 
 <template>
@@ -31,5 +26,4 @@ function handleLogout() {
   </Layout>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
